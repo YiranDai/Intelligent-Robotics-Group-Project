ef5efc217f0d627c4a3aa644e1062e7047069390
Saud Almbdal <saa718@student.bham.ac.uk>
2020-10-14 20:52:32 +0100

    exploring the map. in some cases, the robot hits the walls

--- assignment0/obstacle_avoidance.py
+++ assignment0/obstacle_avoidance.py
@@ -3,39 +3,62 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+#moving = True
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=100)
+base_data = Twist()
+
+
 
 def move():
-
-    # rate = rospy.Rate(10) #10hz
-
+    rospy.init_node('Mover', anonymous=True)
     rospy.Subscriber("base_scan",LaserScan , callback)
-
+    rospy.spin()
 
 def callback(data):
-  #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data
+    #global moving
+    base_data.linear.x = 0.0
+    left = (-1, sum(data.ranges[:100]), 0)
+    upper_left = (-0.5, sum(data.ranges[100:200]), 1)
+    forward = (0, sum(data.ranges[200:300]), 2)
+    upper_right = (0.5, sum(data.ranges[300:400]), 3)
+    right = (1, sum(data.ranges[400:]), 4)
+    directions = [left, upper_left, forward, upper_right, right]
+    direction = max(directions, key=lambda x: x[1])
 
+    narrowed_range = direction[2] * 100
+    angular = data.ranges[narrowed_range : narrowed_range+100]
 
-   angularZ = get_direction(data)
+    base_data.angular.z = direction[0]
 
+    if min(angular[80:]) < 1.5 and min(angular[80:]) < 1.5:
+        base_data.angular.z = 10
+    elif min(angular[80:]) < 1 :
+        base_data.angular.z += -0.6
+    elif min(angular[:20]) < 1:
+        base_data.angular.z += 0.6
+    else:
+        base_data.linear.x = 0.1
 
-   pub = rospy.Publisher('cmd_vel',Twist, queue_size=100)
-   rospy.init_node('Mover', anonymous=True)
-   base_data = Twist()
-   base_data.linear.x = 0.1
-   base_data.angular.z = 0.1
-   pub.publish(base_data)
+    '''
+    if min(angular[80:]) < 1 :
+        base_data.angular.z += -0.2
+        if moving:
+            moving = False
+        else:
+            base_data.angular.z = 60
+    elif min(angular[:20]) < 1:
+        base_data.angular.z += 0.2
+        if moving:
+            moving = False
+        else:
+            base_data.angular.z = 60
+    else:
+        base_data.linear.x = 0.2
+        if moving == False:
+            moving = True
+            '''
 
-
-def get_direction(data):
-
-    # value at 0 degrees
-
-    left = data.ranges[0]
-    # value at 90 degrees
-    front= data.ranges[len(data.ranges)/2]
-    # value at 180 degrees
-    right= data.ranges[len(data.ranges)-1]
-
+    pub.publish(base_data)
 
 
 
