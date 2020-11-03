0375f3b7639e86c21488268fd59b53ab02d0948f
Saud Almbdal <saa718@student.bham.ac.uk>
2020-11-03 03:20:07 +0000

    fixing update and estimate methods

--- assignment0/talker.py
+++ assignment0/talker.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import rospy
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+
 def talker():
+
+	rospy.Subscriber("base_scan",LaserScan , callback)
+
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size=100)
 	rospy.init_node('Mover', anonymous=True)
 	# rate = rospy.Rate(10) #10hz
@@ -12,6 +17,12 @@ def talker():
 		base_data.angular.z = 0.2
 		pub.publish(base_data)
 		# rate.sleep()
+
+def callback(data):
+  #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data
+
+  print((data.ranges[0]))
+
 if __name__ == '__main__':
 	try:
 		talker()
