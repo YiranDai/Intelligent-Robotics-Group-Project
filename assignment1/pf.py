8de93d8d280bcddb7f0116a2387a5c4647eff9c8
Motaz Alshaikh <msa722@student.bham.ac.uk>
2020-11-03 02:24:32 +0000

    fixing estimate and update methods

--- assignment1/pf.py
+++ assignment1/pf.py
@@ -4,10 +4,22 @@ import math
 import rospy
 
 from util import rotateQuaternion, getHeading
-from random import random
-
+#from random import random
+import random
 from time import time
 
+
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+
+    return num
 
 class PFLocaliser(PFLocaliserBase):
 
@@ -34,20 +46,24 @@ class PFLocaliser(PFLocaliserBase):
         :Return:
             | (geometry_msgs.msg.PoseArray) poses of the particles
         """
+        '''
         rospy.loginfo(type(initialpose))
         rospy.loginfo(initialpose)
-
+        '''
         # orientation, covariance, noise
         poses = PoseArray()
         for i in range(self.NUMBER_PREDICTED_READINGS):
             pose = Pose()
             pose.orientation = initialpose.pose.pose.orientation
-            pose.position.x = initialpose.pose.pose.position.x + random()
-            pose.position.y = initialpose.pose.pose.position.y + random()
+            pose.position.x = initialpose.pose.pose.position.x + random.randint(-10,10)
+            pose.position.y = initialpose.pose.pose.position.y + random.randint(-10,10)
             pose.position.z = initialpose.pose.pose.position.z
             poses.poses.append(pose)
 
+        rospy.loginfo("-----------------------poses----------------")
         rospy.loginfo(poses)
+        rospy.loginfo("-----------------------endposes----------------")
+
         return poses
 
 
@@ -60,31 +76,43 @@ class PFLocaliser(PFLocaliserBase):
             | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
 
          """
-        rose.loginfo("update particle cloud")
+        #rospy.loginfo("out-dated particle cloud")
+        #rospy.loginfo(self.particlecloud.poses)
          # idk, update particle filter based on sensor model
-        poses = self.particlecloud
+        poses = self.particlecloud.poses
         # 1- assign w self.sensor_model.get_weight()
         weights = []
-        for pose in poses:
-            weights.append(self.sensor_model.get_weight(self,scan,pose))
+        for i in range(len(poses)):
+            weights += [self.sensor_model.get_weight(scan, poses[i])]
 
+        #rospy.loginfo(sum(weights))
+        normWeights = [float(i)/sum(weights) for i in weights]
         # 2- resample particles
-        resampled=[]
-        c=[weights[0]]
-        for i in range(1,len(weights)):
-            c[i]= c[i-1]+weights[i]
-        u = random.random(0,1/len(weights))
-
+        resampled=PoseArray()
+        c=[normWeights[0]]
+        for i in range(1,len(normWeights)):
+            c += [c[i-1]+normWeights[i]]
+        u = random.uniform(0.0, 1.0/len(normWeights))
+        #rospy.loginfo('c: ')
+        #ospy.loginfo(c)
+        #rospy.loginfo('u: ')
+        #rospy.loginfo(u)
         i = 0
-        for j in range(len(weights)):
-            while u > c[i]:
-                i+=i
-            resampled.append(poses[i])
-            u += 1/len(weights)
+        for j in range(len(normWeights)):
+            while u > c[i] :
+                i+=1
+            resampled.poses.append(poses[i])
+            u += 1.0/len(normWeights)
+            #rospy.loginfo('u: ')
+            #rospy.loginfo(u)
 
         # 3- self.particlecloud = poses
-        self.particlecloud = resampled
-        rospy.loginfo(self.particlecloud)
+        #rospy.loginfo('------------------------updated poses------------------')
+        self.particlecloud.poses = resampled.poses
+        #rospy.loginfo(self.particlecloud.poses)
+        #rospy.loginfo('-------------------end updated------------')
+
+
     def estimate_pose(self):
         """
         This should calculate and return an updated robot pose estimate based
@@ -101,18 +129,11 @@ class PFLocaliser(PFLocaliserBase):
         :Return:
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
-        rospy.loginfo(most_frequent(self.particlecloud))
-        return most_frequent(self.particlecloud)
 
 
-    def most_frequent(List):
-        counter = 0
-        num = List[0]
+        rospy.loginfo('--------------------estimatedpose-------------')
+        rospy.loginfo(most_frequent(self.particlecloud.poses))
+        rospy.loginfo('--------------------end estimatedpose-------------')
 
-        for i in List:
-            curr_frequency = List.count(i)
-            if(curr_frequency> counter):
-                counter = curr_frequency
-                num = i
+        return most_frequent(self.particlecloud.poses)
 
-        return num
